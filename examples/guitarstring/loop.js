3ab4403b26c4f1683e1db9f3492a12469ad462cd
Aatish Bhatia <aatish@gmail.com>
2018-06-15 11:53:43 -0400

    neatened

--- examples/guitarstring/loop.js
+++ examples/guitarstring/loop.js
@@ -1,39 +1,31 @@
-let noiseWave, movingWave;
+let noiseWave;
 
 function setup() {
   noiseWave = new Float32Array(128)
     .map(e => whiteNoise())
     .map(e => 0.1 * e);
+}
 
-  movingWave = noiseWave.slice();
+function loop(input, output) {
+
+  const amp = 0.1;
+  noiseWave = noiseWave.map(delay(1)).map(average);
+
+  for (let i = 0; i < numSamples; i++){
+    output[i] = noiseWave[i];
+  }
 }
 
 function whiteNoise() {
   return (2 * Math.random() - 1);
 }
 
-function oneZero(input, b1 = 0.5, b2 = -0.5) {
-  let output = input.slice();
-
-  for (let i = 0; i < numSamples; i++) {
-    if (i > 0){
-      output[i] = b1 * input[i] + b2 * input[i - 1];
-    } else {
-      output[i] = b1 * input[i];
-    }
-  }
-
-  return output;
-}
-
 const delay = m => (e, i, x) => x[(i + m) % numSamples];
 
-function loop(input, output) {
-
-  const amp = 0.1;
-  movingWave = movingWave.map(delay(1)).applyFilter(oneZero, 0.5, 0.5);
-
-  for (let i = 0; i < numSamples; i++){
-    output[i] = movingWave[i];
+const average = (e, i, x) => {
+  if (i > 0) {
+    return 0.5 * (x[i] + x[i - 1]);
+  } else {
+    return e;
   }
-}
+};
